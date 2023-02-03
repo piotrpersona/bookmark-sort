07f0ebcfbc6d8ea742fc556b13ad1e90b6c31c09
Piotr Persona <persona.piotr@gmail.com>
2023-02-03 12:20:07 +0100

    feat: Cleanup
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -63,39 +63,6 @@ const App: FC = () => {
     })
   }
 
-  const treeData = [
-    {
-      value: 'parent 1',
-      title: 'parent 1',
-      children: [
-        {
-          value: 'parent 1-0',
-          title: 'parent 1-0',
-          children: [
-            {
-              value: 'leaf1',
-              title: 'my leaf',
-            },
-            {
-              value: 'leaf2',
-              title: 'your leaf',
-            },
-          ],
-        },
-        {
-          value: 'parent 1-1',
-          title: 'parent 1-1',
-          children: [
-            {
-              value: 'sss',
-              title: <b style={{ color: '#08c' }}>sss</b>,
-            },
-          ],
-        },
-      ],
-    },
-  ];
-
   return (
     <div className="App">
       <Select
